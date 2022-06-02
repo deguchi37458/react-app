147e6abe473822e3b57977e3740ba5ed725aa69d
deguchi <deguchi37458@gmail.com>
2022-06-03 08:27:23 +0900

    init

---
+++ src/components/ColoredMessage.jsx
@@ -0,0 +1,13 @@
+export const ColoredMessage = (props) => {
+
+  const {color, children} = props;
+
+  const contentStyle = {
+    color,
+    fontSize: "20px"
+  };
+  return (
+    <h1 style={ contentStyle }>{children}</h1>
+  );
+}
+
